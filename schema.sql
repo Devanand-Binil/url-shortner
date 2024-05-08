5432427daae1a91d78c55b91806e5c3fe23dea12
Devanand-Binil <devanand.b.1369@gmail.com>
2024-05-08 14:58:47 +0530

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@
 
 CREATE TABLE IF NOT EXISTS urls (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    created TIMESTAMP NOT NULL DEFAULT CONVERT_TZ(CURRENT_TIMESTAMP(), 'UTC', 'Asia/Kolkata'),
+    created TIMESTAMP NOT NULL DEFAULT (datetime('now', 'localtime', '+5 hours', '+30 minutes')),
     original_url TEXT ,
     last_updated TEXT ,
     referrer TEXT,
